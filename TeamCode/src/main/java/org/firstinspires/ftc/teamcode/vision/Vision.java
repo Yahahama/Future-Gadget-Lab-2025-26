a35a4e211650be4cbe6367dd4270524e4f72aaab
Amika <92111982+Yahahama@users.noreply.github.com>
2025-09-28 13:15:35 -0400

    Uhh vision stuff?

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Vision.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.subsystems.CameraSubsystem;
+
+@Config
+@TeleOp(name = "Vision =)", group = "Vision")
+public class Vision extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
