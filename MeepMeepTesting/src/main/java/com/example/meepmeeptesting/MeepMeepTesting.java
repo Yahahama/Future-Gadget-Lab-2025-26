99840caaa49501410c93ec70b926f054b30baa2b
Bokoooooo <brooklin20201@gmail.com>
2025-10-05 14:24:16 -0400

    Added the positions of the Goals, Starting Positions, and the Artifacts

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -18,14 +18,60 @@ public class MeepMeepTesting {
     // This "Positions" class is copied from Autonomous.java and should be kept up to date
     // by copy-pasting. If you can figure out how to just import Positions from Autonomous,
     // please do that.
+
+    //IMPORTANT: READ THE ANNOTATIONS FOR THE ENUMS
     public static class Positions {
+
+        //Note: These are APPROXIMATE POSITIONS
+
         enum GOAL {
-            RED(new Pose2d(48, -48, Math.toRadians(135))),
-            BLUE(new Pose2d(-48, -48, Math.toRadians(225)));
+            RED(new Pose2d(-64, 57, Math.toRadians(135))),
+            BLUE(new Pose2d(-64, -57, Math.toRadians(225)));
 
             private Pose2d pose2d;
 
             GOAL(Pose2d _pose2d) {
+                this.pose2d = _pose2d;
+            }
+
+            public Pose2d getPose() {
+                return pose2d;
+            }
+        }
+
+        enum START {
+
+            //RED_CLOSE means the starting position CLOSE TO THE GOAL
+
+            RED_CLOSE(new Pose2d(-49.3f, 50.3f, Math.toRadians(315f))),
+            RED_FAR(new Pose2d(62, 16, Math.toRadians(180))), //y pos + 12???
+            BLUE_CLOSE(new Pose2d(-49.3f, -50.3f, Math.toRadians(45))),
+            BLUE_FAR(new Pose2d(62, -16, Math.toRadians(180))); //y pos - 12???
+
+            private Pose2d pose2d;
+
+            START(Pose2d _pose2d) {
+                this.pose2d = _pose2d;
+            }
+
+            public Pose2d getPose() {
+                return pose2d;
+            }
+        }
+        enum ARTIFACT {
+
+            //+-5 for the other artifacts near the middle one
+
+            RED_A(new Pose2d(-11.5f, 46.5f, Math.toRadians(315f))),
+            RED_B(new Pose2d(12.3f, 46.5f, Math.toRadians(180))),
+            RED_C(new Pose2d(36, 46.5f, Math.toRadians(180))),
+            BLUE_A(new Pose2d(-11.5f, -46.5f, Math.toRadians(45))),
+            BLUE_B(new Pose2d(12.3f, -46.5f, Math.toRadians(45))),
+            BLUE_C(new Pose2d(36, -46.5f, Math.toRadians(180)));
+
+            private Pose2d pose2d;
+
+            ARTIFACT(Pose2d _pose2d) {
                 this.pose2d = _pose2d;
             }
 
@@ -71,7 +117,7 @@ public class MeepMeepTesting {
 //            .build();
 
         Image img = null;
-        try { img = ImageIO.read(new File("/Users/abibolov27/Documents/Images/Robotics/DecodeMeepMeepBackground.png")); }
+        try { img = ImageIO.read(new File("/Users/bma30/Documents/FTC/DecodeMeepMeepBackground.png")); }
         catch (IOException e) {}
 
         meepMeep.setBackground(img)
